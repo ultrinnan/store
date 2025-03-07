9f195a983b3f38cab58f0ac389d5adf2b6407756
Serhii Fedirko <serhii.fedirko@gmail.com>
2025-03-07 15:09:59 +0000

    integrate styles and header font

--- wp-content/themes/veldrin/functions.php
+++ wp-content/themes/veldrin/functions.php
@@ -2,28 +2,23 @@
 
 function f_scripts_styles()
 {
-//    wp_enqueue_style('f_style', get_template_directory_uri() . '/../veldrin/css/main.min.css');
+    wp_enqueue_style('f_style', get_template_directory_uri() . '/../veldrin/css/main.min.css');
     wp_enqueue_style('style', get_template_directory_uri() . '/../veldrin/style.css');
 
 //    wp_enqueue_script('f_scripts', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '1.0', true);
 }
-// Create action where we connected scripts and styles in function f_scripts_styles
-add_action('wp_enqueue_scripts', 'f_scripts_styles', 1);
+add_action('wp_enqueue_scripts', 'f_scripts_styles');
 
-add_theme_support('menus');
+function custom_theme_setup() {
+    add_theme_support('menus');
 
-//custom locations for menus
-function f_register_menus() {
-    register_nav_menu( 'header', __( 'Header menu', 'theme-slug' ) );
-    register_nav_menu( 'footer', __( 'Footer menu', 'theme-slug' ) );
-}
-add_action( 'after_setup_theme', 'f_register_menus' );
-
-function my_child_theme_add_woocommerce_support() {
     add_theme_support('woocommerce');
     // Enable product gallery features (optional but recommended)
     add_theme_support('wc-product-gallery-zoom');
     add_theme_support('wc-product-gallery-lightbox');
     add_theme_support('wc-product-gallery-slider');
+
+    register_nav_menu( 'header', __( 'Header menu', 'theme-slug' ) );
+    register_nav_menu( 'footer', __( 'Footer menu', 'theme-slug' ) );
 }
-add_action('after_setup_theme', 'my_child_theme_add_woocommerce_support');
+add_action('after_setup_theme', 'custom_theme_setup');
